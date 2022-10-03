bc44811c8b74b7bb2b8f301f9f4e4bf890ff709d
Andrews Essilfie <essilfie@ualberta.ca>
2022-10-03 13:10:04 -0600

    fixed secret key issue

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,7 +11,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
+
+env = environ.Env()
+# reading the .env file
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&p6h+wo4l3%ip&^76_m@qj04&o6q8fj9vbojlbgs@yt#xn@v8-"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
